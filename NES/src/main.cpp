f2d94b9f39ea819818972dd0a4b825d3f0de087f
Anthony Polka <anthonypolka@gmail.com>
2025-01-04 11:45:01 -0600

    maid main blank

--- NES/src/main.cpp
+++ NES/src/main.cpp
@@ -1,32 +1,8 @@
 
 
-// #include "rom.h"
-// #include "MOS6502.h"
 
 int main()
 {
-    // CPU::MOS6502 c(nullptr, nullptr);
-
-    // c.print_instruction_set ();
-    // NES_ROM rom ("../Super_mario_brothers.nes");
-    // NES_ROM rom ("../zelda.nes");
-    // NES_ROM rom("../Tetris.nes");
-    // NES_ROM rom("../contra.nes");
-
-    // auto read = [&rom](std::uint16_t address, std::uint8_t& data)
-    // {
-    //     return rom.cpu_read(address, data);
-    // };
-
-    // auto read = [&rom](auto address)
-    // {
-    //     return rom.get_prg_memory()[address];
-    // };
-
-    // CPU::MOS6502 cpu(read, nullptr);
-    // cpu.decompile(rom.size());
-
-
-
+   
     return 0;
 }
